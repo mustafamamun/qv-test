ddb6da32d09b7782edf5eccf18176fd6c93d5492
khondmu <mustafa.khondkar@tecnotree.com>
2017-06-27 16:26:56 +0300

    catalog controller added and working

---
+++ api/controllers/catalog.js
@@ -0,0 +1,165 @@
+/*jslint node:true*/
+'use strict';
+import config from 'config';
+import query from '../services/db/query';
+import _ from 'lodash';
+import { logger } from '../services/util/logger';
+import { ObjectId } from 'mongodb';
+const collectionName = config.get('mongoConfig.catalogCollection');
+// const IdRegExp = new RegExp('/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i');
+const IdRegExp = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;
+module.exports = {
+  getProduct,
+  addProduct,
+  updateProduct,
+  updateProductPartially,
+  deleteProduct
+};
+//Controller function
+function getProduct(req, res, next) {
+  let qs = {}, sort = {};
+  if(req.swagger.params._id.value){
+    if(!IdRegExp.test(req.swagger.params._id.value)){
+      return res.status(400).json({message : 'Bad request, Id is not in write format'});
+    }
+    qs._id = new ObjectId(req.swagger.params._id.value);
+  }
+  if(req.swagger.params.name.value){
+    qs.name = {'$regex' : req.swagger.params.name.value, '$options' : 'i'};
+  }
+  let sortId = req.swagger.params.sort.value;
+  if(sortId){
+    if(sortId === 'name'){
+      sort.name = 1;
+    }
+    if(sortId === 'price'){
+      sort.price = 1;
+    }
+  }
+  let priceRange = req.swagger.params.priceRange.value;
+  if(priceRange){
+    if(priceRange === 'lessThan5'){
+      qs.unitPrice = {$lt : 5};
+    }
+    if(priceRange === 'from5To10'){
+      qs.unitPrice = {$gt : 5, $lt:10};
+    }
+    if(priceRange === 'greaterThan10'){
+      qs.unitPrice = { $gt : 10 };
+    }
+  }
+  let paging = {
+          limit : req.swagger.params.limit.value || 20,
+          skip : req.swagger.params.offset.value || 0
+      };
+  query.getFromDB(collectionName, qs)
+  .then((result)=>{
+    return query.findCount(collectionName, qs)
+    .then((count)=>{
+        return res.json({total : count, count : result.length, results : result});
+    });
+  }).catch((err)=>{
+    return res.status(500).json(err);
+  });
+
+}
+
+function addProduct(req, res, next){
+  let productDetails = _.assign({}, {name : req.body.name, unitPrice: req.body.unitPrice, quantity : req.body.quantity});
+  let _id = req.body.name_id;
+  if(_id){
+    if(!IdRegExp.test(_id)){
+      return res.status(400).json({message : 'Bad request, Id is not in write format'});
+    }
+    let qs = { _id : new ObjectId(_id)};
+    query.getFromDB(collectionName, qs)
+    .then((result)=>{
+      if(result.length>0){
+        let us = { $inc : {quantity : req.body.quantity}};
+        return query.updateDB(collectionName, qs, us);
+      }else{
+        query.saveToDB(collectionName, productDetails);
+      }
+    })
+    .then((result)=>{
+      res.json(result);
+    })
+    .catch((err)=>{
+      return res.json(err);
+    });
+  }else{
+    query.saveToDB(collectionName, productDetails)
+    .then((result)=>{
+      return res.status(201).json(productDetails);
+    }).catch((err)=>{
+      logger.error(err);
+      return res.status(500).json({message : 'Internal server error'});
+    });
+  }
+
+}
+
+function updateProduct(req, res, next){
+  if(!IdRegExp.test(req.body.id)){
+    return res.status(400).json({message : 'Bad request, Id is not in write format'});
+  }
+  let qs = _.assign({}, {_id : new ObjectId(req.body._id)});
+  let us = _.assign({}, req.body, {_id : new ObjectId(req.body._id)});
+  query.updateDB(collectionName, qs, us)
+  .then((result)=>{
+    if((result.result || {}).n > 0 && (result.result || {}).nModified > 0){
+      return res.status(200).json(us);
+    }else if((result.result || {}).n ===0){
+      return res.status(204).json({messge : 'Contend to be modified not found'});
+    }else if((result.result || {}).nModified === 0){
+      return res.status(500).json({message : 'Could not modify the product'});
+    }else{
+      return res.status(500).json({message : 'Something wrong happend'});
+    }
+
+  })
+  .catch((err)=>{
+    logger.error(err);
+    return res.status(500).json({messge: 'Internal server error'});
+  });
+}
+
+function updateProductPartially(req, res, next){
+  if(!IdRegExp.test(req.swagger.params._id.value)){
+    return res.status(400).json({message : 'Bad request, Id is not in write format'});
+  }
+  let qs = { _id : new ObjectId(req.swagger.params._id.value)};
+  let us = _.pickBy(_.assign({}, {name : req.body.name, unitPrice : req.body.unitPrice, quantity : req.body.quantity}), (prop)=>{
+    return prop !== undefined;
+  });
+  if(_.isEmpty(us)){
+    return res.status(400).json({messge : 'Update parameters are not right'});
+  }
+  query.updateDB(collectionName, qs, us)
+  .then(()=>{ return query.getFromDB(collectionName, qs); })
+  .then((result)=>{
+    if(result.length === 0){
+      return res.status(204).json({message : 'Content not found'});
+    }else{
+      return res.status(200).json(result[0]);
+    }
+  })
+  .catch((err)=>{
+    return res.status(500).json({message : 'Internal server error'});
+  });
+}
+
+
+function deleteProduct(req, res, next){
+  if(!IdRegExp.test(req.swagger.params._id.value)){
+    return res.status(400).json({message : 'Bad request, Id is not in write format'});
+  }
+  let qs =  {_id : new ObjectId(req.swagger.params._id.value)};
+  query.removeFromDB(collectionName, qs)
+  .then((result)=>{
+      return (result.result ||  {}).n > 0 ?  res.status(200).json({message : `Successfully removed product with id ${req.swagger.params._id_value}`}) : res.status(204).json({message : `Content not found with id ${req.swagger.params._id_value}`});
+  }).catch((err)=>{
+    logger.error(err);
+    res.status(500).json({message : 'Internal server error'});
+  });
+}
--- api/services/db/query.js
+++ api/services/db/query.js
@@ -5,9 +5,9 @@ import { db }  from './connection';
 import 	promise from 'bluebird';
 import config from 'config';
 
-function saveToDB(data){
+function saveToDB(collection, data){
 	return new promise(function(resolve, reject){
-		return db.collection(config.get('mongoConfig.collection')).insert(data, function(err, result){
+		return db.collection(collection).insert(data, function(err, result){
 			if(err) {
 				reject(err);
 			}else{
@@ -16,9 +16,9 @@ function saveToDB(data){
 		});
 	});
 }
-function updateDB(qs, us){
+function updateDB(collection, qs, us){
 	return new promise(function(resolve, reject){
-		return db.collection('orders').update(qs, {$set:us}, {upsert : false}, function(err, result){
+		return db.collection(collection).update(qs, {$set:us}, {upsert : false}, function(err, result){
 			if(err) {
 				reject(err);
 			}else{
@@ -27,16 +27,23 @@ function updateDB(qs, us){
 		});
 	});
 }
-function getFromDB(qs, paging, sort){
-	return db.collection(config.get('mongoConfig.collection')).find(qs, {_id:0}, paging).sort(sort).toArray();
+function getFromDB(collection, qs, paging, sort){
+	return db.collection(collection).find(qs, paging).sort(sort).toArray();
 }
 
-function findCount(qs){
- 	return db.collection(config.get('mongoConfig.collection')).find(qs).count();
+function removeFromDB(collection, qs){
+	return db.collection(collection).remove(qs);
 }
+
+function findCount(collection, qs){
+ 	return db.collection(collection).find(qs).count();
+}
+
+
 module.exports = {
 	saveToDB,
 	getFromDB,
 	updateDB,
-	findCount
+	findCount,
+	removeFromDB
 };
