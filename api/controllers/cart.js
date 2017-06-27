18147ce4bb44fbd977ef360ed46b608ba7dccaa2
mustafamamun <mustafamamun@Mustafas-MacBook-Pro.local>
2017-06-28 00:44:49 +0300

    add product to cart finalized

--- api/controllers/cart.js
+++ api/controllers/cart.js
@@ -30,12 +30,22 @@ function addItemToCart(req, res, next){
   let customerId = req.body.customerId;
   query.getFromDB(collectionName, {customerId : customerId})
   .then((result)=>{
+    console.log(result);
     if(result.length > 0){
-      console.log('in');
-      result[0].products.push(_.assign({}, {productId :  req.body.productId, qyantity: req.body.quantity, unitPrice : req.body.unitPrice}));
-      result[0].total = _.sumBy(result[0].products, 'value.unitPrice');
+      result[0].products.push(_.assign({}, {productId :  req.body.productId, quantity: req.body.quantity, unitPrice : req.body.unitPrice}));
+      console.dir(result[0].products, {depth:null});
+      result[0].products = _
+       .chain(result[0].products)
+       .groupBy('productId')
+       .map((value, key)=>{
+         return {productId : key, quantity : _.sumBy(value, (eachValue)=>{ return eachValue.quantity; }), unitPrice : value[0].unitPrice};
+       })
+       .value();
+      console.dir(result[0].products, {depth:null});
+      result[0].total = _.sumBy(result[0].products, (eacheProduct)=>{
+        return eacheProduct.unitPrice*eacheProduct.quantity;
+      });
       responseObj = result[0];
-      console.dir(responseObj, {depth : null});
       return query.updateDB(collectionName, {customerId:customerId}, {$set : {products : result[0].products, total : result[0].total}});
     }else{
       let cartDetails = _.assign({}, {customerId : customerId, products : [{productId :  req.body.productId, qyantity: req.body.quantity, unitPrice : req.body.unitPrice}], total: req.body.unitPrice});
